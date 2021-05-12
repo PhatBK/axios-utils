3acd6259dc220f0ca5b1d6ff55a56ca0bbaff95b
Nguyen Huy Phat <phatSN11081996@gmail.com>
2021-05-12 10:21:12 +0700

    Create create.instance.js

---
+++ create.instance.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+let BASE_TEST = ""; // base url same: https://github.com/
+let axiosInstance = {}; // object save one  axios instance for app
+
+const errorHandler = (error) => {
+  // todo handler when error
+  return Promise.reject({ ...error })
+};
+
+const successHandler = (response) => {
+   // todo handler when response success
+   return response
+};
+
+const setAxiosInstance = (timeout) => {
+    /**
+     * create axios instance
+     * */
+    axiosInstanceTest = axios.create({
+        baseURL: BASE_TEST,
+        timeout: timeout,
+        withCredentials: true,
+    });
+
+    /**
+     * add value before for request
+     * */
+    axiosInstanceTest.interceptors.request.use(function (config) {
+        config.headers["Request-Id"] = ""; // generate request id
+        config.requestTime = Date.now();
+        return config;
+    });
+
+    /**
+     * add after for response
+     * */
+    axiosInstanceTest.interceptors.response.use(
+        response => successHandler(response),
+        error => errorHandler(error)
+    )
+};
+
+export {
+  axiosInstance,
+  setAxiosInstance
+}
